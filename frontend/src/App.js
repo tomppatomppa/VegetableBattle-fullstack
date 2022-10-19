2d57ebc89254dbecdef230726910d70c33b329af
tomi <tomiwest@hotmail.com>
2022-10-19 10:58:12 +0300

    delete app.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import { useRef } from 'react'
-import './App.css'
 import Game from './components/Game'
 import Higscores from './components/Higscores'
 import Notification from './components/Notification'
